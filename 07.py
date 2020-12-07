4395f7f42107ceac9e8d59c6c024e76506d5e5f3
Mark Marlow <L150609@westpacgroup.com>
2020-12-08 09:29:54 +1100

    finished day 7, still have an off-by-1 error �

---
+++ 07.py
@@ -0,0 +1,89 @@
+def process_rules(raw):
+    output = {}
+    for row in raw:
+        parent, children = row.split(" contain ")
+        parent = parent.replace("bags", "bag")
+        children = process_children(children)
+        output[parent] = children
+    return output
+def process_children(children):
+    output = []
+    for child in children.split(", "):
+        child = child.strip().replace("bags", "bag")
+        if child == "no other bag.":
+            return []
+        num = child.split(" ")[0]
+        if num == "no":
+            num_val = 0
+        else:
+            num_val = int(num)
+        child = child.replace(num, "").strip()
+        output.append({"name": child.replace(".",""), "n": num_val})
+    return output
+
+def traverse_children(children, current_node, matching_nodes, tree):
+    for child in children:
+        children_of_children = tree[child["name"]]
+        if len(children_of_children) == 0 or bag_found(children_of_children, matching_nodes, current_node):
+            continue
+        traverse_children(children_of_children, current_node, matching_nodes, tree)
+
+def bag_found(children, matching_nodes, current_node):
+    bags = [child["name"] for child in children]
+    if "shiny gold bag" in bags:
+        matching_nodes.append(current_node)
+        return True
+    return False
+
+def search_tree(tree):
+    matching_nodes = []
+    for node, children in tree.items():
+        if bag_found(children, matching_nodes, node):
+            continue
+        traverse_children(children, node, matching_nodes, tree)
+    
+    return matching_nodes
+
+inputs = open("inputs/07.txt").readlines()
+data = process_rules(inputs)
+def part1():
+    matches = search_tree(data)
+    print(matches)
+    
+def count_bags(list_of_bags, tree):
+    if list_of_bags == []:
+        return 1
+    total = 1
+    for bag in list_of_bags:
+        next_bag = tree[bag["name"]]
+        this_bag_count = bag["n"]
+        total += this_bag_count * count_bags(next_bag, tree)
+    return total
+
+def part2():
+    head = data["shiny gold bag"]
+    bags = count_bags(head, data)
+    print(bags)
+
+data = open('inputs/07.txt', 'r').read().split('\n')
+
+m = {}
+
+for line in data:
+	line = line.replace('bags', '').replace('bag', '').strip('.')
+	line = line.split('contain')
+	m[line[0].strip()] = line[1].strip().split(',')    
+
+def get_bags(bag_col):
+
+	if 'no other' in m[bag_col]:
+		return 1
+
+	total = 1
+	for col in m[bag_col]:
+		x = col.split()
+		total += int(x[0]) * get_bags(' '.join(x[1:]))
+
+	return total
+
+print(get_bags('shiny gold')-1)
